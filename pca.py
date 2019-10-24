2115845cc7cbac219be38863436b470d630d7044
Michał Myller <michalmyll@gmail.com>
2019-10-24 22:27:02 +0200

    added pca visualization, added scatter correlation plots

---
+++ pca.py
@@ -0,0 +1,25 @@
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+import matplotlib.pyplot as plt
+
+from utils import load_data_frame
+
+data, labels = load_data_frame(r"processed.cleveland.data")
+
+# Normalize data
+scaler = StandardScaler()
+data = scaler.fit_transform(data)
+
+# Visualize using PCA
+data_after_pca = PCA(n_components=2).fit_transform(data)
+fig, ax = plt.subplots()
+ax.scatter(data_after_pca[labels == 0, 0], data_after_pca[labels == 0, 1], c='red', label='Class 1 (No disease)')
+ax.scatter(data_after_pca[labels > 0, 0], data_after_pca[labels > 0, 1], c='blue', label='Class 2 (Some kind of disease)')
+plt.xlabel('Component 1')
+plt.ylabel('Component 2')
+ax.legend()
+plt.show()
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+
+def load_data_frame(path_to_file: str):
+    """
+    Load the dataset into two container: one with the actual data and the second
+    one with the respective labels
+    :param path_to_file: Path to the .data file
+    :return: numpy array with the data, numpy array with the labels
+    """
+    with open(path_to_file) as file:
+        df = pd.read_csv(file, header=0, na_values='?')
+    labels = df.iloc[:, -1]
+    df = df.fillna(0)
+    df.drop(df.columns[len(df.columns) - 1], axis=1, inplace=True)
+    return np.array(df, dtype=np.float32), np.array(labels, dtype=np.uint8)
