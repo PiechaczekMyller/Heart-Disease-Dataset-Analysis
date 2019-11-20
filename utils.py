2d04e45652fa06221adabc29fb78a579e0ec706e
Michał Myller <michalmyll@gmail.com>
2019-11-20 20:40:39 +0100

    features description and some scripts

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 
-def load_data_frame(path_to_file: str):
+def load_numpy(path_to_file: str):
     """
     Load the dataset into two container: one with the actual data and the second
     one with the respective labels
@@ -15,3 +15,18 @@ def load_data_frame(path_to_file: str):
     df = df.fillna(0)
     df.drop(df.columns[len(df.columns) - 1], axis=1, inplace=True)
     return np.array(df, dtype=np.float32), np.array(labels, dtype=np.uint8)
+
+
+def load_dataframe(path_to_file: str):
+    """
+    Load the dataset into two container: one with the actual data and the second
+    one with the respective labels
+    :param path_to_file: Path to the .data file
+    :return: numpy array with the data, numpy array with the labels
+    """
+    with open(path_to_file) as file:
+        df = pd.read_csv(file, header=0, na_values='?')
+    labels = df.iloc[:, -1]
+    df = df.fillna(0)
+    df.drop(df.columns[len(df.columns) - 1], axis=1, inplace=True)
+    return df, labels
